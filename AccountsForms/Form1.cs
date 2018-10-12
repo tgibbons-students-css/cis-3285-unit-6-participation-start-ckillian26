2051909fe38cfcda4db56da53ef58cb72a3d2593
Chris Killian <ckillian@css.edu>
2018-10-11 23:47:05 -0500

    Defined user interface and completed the deposit button.

--- AccountsForms/Form1.cs
+++ AccountsForms/Form1.cs
@@ -1,31 +1,20 @@
-﻿using Domain;
-using Services;
-using System;
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace AccountsForms
 {
     public partial class Form1 : Form
     {
-        IAccountServices accService = new AccountService();
         public Form1()
         {
             InitializeComponent();
-        }
-
-        private void btnAddAccount_Click(object sender, EventArgs e)
-        {
-            string accountName = txtAccountName.Text;
-            listBoxAccounts.Items.Add(accountName);
-            accService.CreateAccount(accountName, AccountType.Silver);
-        }
-
-        private void listBoxAccounts_SelectedIndexChanged(object sender, EventArgs e)
-        {
-            string accName = listBoxAccounts.SelectedItem.ToString();
-            decimal balance = accService.GetAccountBalance(accName);
-
-            txtBalance.Text = balance.ToString();
         }
     }
 }
